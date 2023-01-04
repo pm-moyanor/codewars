20ba4875a1eb4351657a7160a3f2a7ba67a57e70
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-01-04 18:25:37 -0500

    7 level kata solved

--- cw.js
+++ cw.js
@@ -1,20 +1,33 @@
-function cyclops(n) {
-  const binary = n.toString(2).split("");
-  const arr = [];
+// function cyclops(n) {
+//   const binary = n.toString(2).split("");
+//   const arr = [];
 
-  if (binary.length % 2 === 0 || binary.length <= 1) {
-    return false;
+//   if (binary.length % 2 === 0 || binary.length <= 1) {
+//     return false;
+//   }
+//   binary.map(e => {
+//     if (e === "0") {
+//       arr.push(e);
+//     }
+//   })
+
+// return binary[Math.floor((binary.length - 1) / 2)] === "0" && arr.length < 1
+
+// }
+
+// console.log(cyclops(13));
+// console.log(cyclops(23));
+// console.log(cyclops(27));
+
+function collatz(n) {
+  const sequence = [];
+
+  while (n > 1) {
+    n % 2 === 0 ? (n = n / 2) : (n = n * 3 + 1);
+    sequence.push(n);
   }
-  binary.map(e => {
-    if (e === "0") {
-      arr.push(e);
-    }
-  })
-
-return binary[Math.floor((binary.length - 1) / 2)] === "0" && arr.length < 1
-
+  return sequence.length + 1;
 }
 
-console.log(cyclops(13));
-console.log(cyclops(23));
-console.log(cyclops(27));
+console.log(collatz(20)); //8
+console.log(collatz(15)); //18
