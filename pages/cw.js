9cde49711d7f59a8e57352cbe87a356cf99a18dc
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-03-08 09:39:56 -0500

    add OOP meta-assignment

---
+++ pages/cw.js
@@ -0,0 +1,32 @@
+// Task 1: Build a function-based console log message generator
+
+function consoleStyler(color, background, fontSize, txt ) {  
+
+    var message = "%c" + txt;  
+   
+    var style = `color: ${color};`  
+   
+   style += `background: ${background};`  
+   
+   style += `font-size: ${fontSize};`  
+   
+    console.log(message, style);
+   
+   }  
+
+   consoleStyler('black', 'red', '24px', 'rider')
+   
+   console.log("aa")
+   // Task 2: Build another console log message generator
+   function celebrateStyler() {
+       
+   }
+   
+   // Task 3: Run both the consoleStyler and the celebrateStyler functions
+   
+   
+   // Task 4: Insert a congratulatory and custom message
+   function styleAndCelebrate() {
+       
+   }
+   // Call styleAndCelebrate
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,186 @@
-import { useState } from 'react';
+import { useState } from "react";
 function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>;
+  return <h1>{title ? title : "Default title"}</h1>;
 }
 
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
 
   const [likes, setLikes] = useState(0);
 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
+  //////////////////////Classes Asignment-META
+
+  class Animal {
+    constructor(color = 'yellow', energy = 100) {
+        this.color = color;
+        this.energy = energy;
+    }
+    isActive() {
+        if(this.energy > 0) {
+            this.energy -= 20;
+            console.log('Energy is decreasing, currently at:', this.energy)
+        } else if(this.energy == 0){
+            this.sleep();
+        }
+    }
+    sleep() {
+        this.energy += 20;
+        console.log('Energy is increasing, currently at:', this.energy)
+    }
+    getColor() {
+        console.log(this.color)
+    }
+}
+
+class Cat extends Animal {
+    constructor(sound = 'purr', canJumpHigh = true, canClimbTrees = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canClimbTrees = canClimbTrees;
+        this.canJumpHigh = canJumpHigh;
+    }
+    makeSound() {
+        console.log(this.sound);
+    }
+}
+
+class Bird extends Animal {
+    constructor(sound = 'chirp', canFly = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canFly = canFly;
+    }
+    makeSound() {
+        console.log(this.sound);
+    }
+}
+
+class HouseCat extends Cat {
+    constructor(houseCatSound = "meow", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.houseCatSound = houseCatSound;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        console.log(this.houseCatSound);
+    }
+}
+
+class Tiger extends Cat {
+    constructor(tigerSound = "Roar!", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.tigerSound = tigerSound;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        console.log(this.tigerSound);
+    }
+}
+
+class Parrot extends Bird {
+    constructor(canTalk = false, sound,canFly, color,energy) {
+        super(sound,canFly, color,energy);
+        this.canTalk = canTalk;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        if (this.canTalk) {
+            console.log("I'm a talking parrot!");
+        }
+    }
+}
+
+var fiji = new Parrot(false); // we're passing `false` to the constructor so that fiji can't talk
+var polly = new Parrot(true); // we're passing `true` to the constructor so that polly can talk
+
+fiji.makeSound(); // undefined
+fiji.makeSound(true); // chirp
+
+polly.makeSound(); // I'm a talking parrot!
+polly.makeSound(true); // chirp, I'm a talking parrot!
+
+polly.color; // yellow
+polly.energy; // 100
+
+polly.isActive(); // Energy is decreasing, currently at: 80
+
+var penguin = new Bird("shriek", false, "black and white", 200); // setting all the custom properties
+penguin; // Bird {color: 'black and white', energy: 200, sound: 'shriek', canFly: false }
+
+penguin.sound; // 'shriek'
+penguin.canFly; // false
+penguin.color; // 'black and white'
+penguin.energy; // 200
+penguin.isActive(); // Energy is decreasing, currently at: 180
+
+var leo = new HouseCat();
+
+// leo, no purring please:
+leo.makeSound(false); // meow
+// leo, both purr and meow now:
+leo.makeSound(true); // purr, meow
+
+var cuddles = new Tiger();
+cuddles.makeSound(false); // Roar!
+cuddles.makeSound(true); // purr, Roar!
+
+
+////////////////////////////////////////////////////////////////////////////
+// Task 1: Build a function-based console log message generator
+
+// function consoleStyler(color, background, fontSize, txt ) {  
+
+//   var message = "%c" + txt;  
+ 
+//   var style = `color: ${color};`  
+ 
+//  style += `background: ${background};`  
+ 
+//  style += `font-size: ${fontSize};`  
+ 
+//   console.log(message, style);
+ 
+//  }  
+ 
+ 
+ 
+//  // Task 2: Build another console log message generator
+//  function celebrateStyler(reason) {
+//      var fontStyle = "color: tomato; font-size: 50px";
+//      if (reason == "birthday") {
+//          console.log(`%cHappy birthday`, fontStyle);
+//      } else if (reason == "champions") {
+//          console.log(`%cCongrats on the title!`, fontStyle);
+//      } else {
+//          console.log(message, style)
+//      }
+     
+//  }
+ 
+ 
+ 
+//  // Task 3: Run both the consoleStyler and the celebrateStyler functions
+//  consoleStyler("#1d5c63", "#ede6db", "40px", "Congrats!");
+//  celebrateStyler('champions')
+ 
+//  // Task 4: Insert a congratulatory and custom message
+//  function styleAndCelebrate(func1,func2) {
+//      console.log(func1,func2)
+//  }
+//  // Call styleAndCelebrate
+ 
+//    styleAndCelebrate(consoleStyler('#ef7c8e','#fae8e0','30px','You made it!'), celebrateStyler('champions'))
+
+//  // consoleStyler("#1d5c63", "#ede6db", "40px", "Congrats!");
+//   function handleClick() {
+//     setLikes(likes + 1);
+//   }
 
   return (
     <div>
